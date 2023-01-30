001be91ff9165450b6632b0ccb01ff611cb91e49
haile21 <hailishmit@gmail.com>
2023-01-30 22:26:09 +0300

    unit test fot the service class added

--- src/test/java/com/addistutor/covid19CaseRecord/Covid19CaseRecordApplicationTests.java
+++ src/test/java/com/addistutor/covid19CaseRecord/Covid19CaseRecordApplicationTests.java
@@ -5,9 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
